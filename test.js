524b3b7dc0a15c0960e28e7f26e0ace30bec15c4
baleian <baleian90@gmail.com>
2016-07-15 10:41:14 +0900

    argument로 실행 제어 완료

--- test.js
+++ test.js
@@ -1,85 +1,133 @@
 const fs = require('fs');
 const spawn = require('child_process').spawn;
 
-const gdb = spawn('gdb', ['a.out']);
-gdb.stdin.write('display K\n');
-gdb.stdin.write('display cnt\n');
-gdb.stdin.write('display card\n');
-gdb.stdin.write('set listsize 1\n');
-gdb.stdin.write('break main\n');
-gdb.stdin.write('run < input.txt\n');
 
-var run = true;
-var logging = false;
-var buf = new String();
-const log = fs.createWriteStream('output.log', {flag: 'w'});
+const argv = (function parseProcessArgument() {
+        var binaryfile = process.argv[2];
+        var targets = [];
+        var breaks = [];
+        var inputTxt;
 
-var mode = 0;
+        var mode = 0;
+        for (var i = 3; i < process.argv.length; i++) {
+                if (process.argv[i] === '--targets' || process.argv[i] === '-t') {
+                        mode = 1;
+                }
+                else if (process.argv[i] === '--breaks' || process.argv[i] === '-b') {
+                        mode = 2;
+                }
+                else if (process.argv[i] === '--input' || process.argv[i] == '-i') {
+                        mode = 0;
+                        inputTxt = process.argv[++i];
+                }
+                else {
+                        if (mode == 1) targets.push(process.argv[i]);
+                        if (mode == 2) breaks.push(process.argv[i]);
+                }
+        }
 
-gdb.stdout.on('data', (data) => {
-	if (!run) return;
+        return {
+                binaryfile: binaryfile,
+                targets: targets,
+                breaks: breaks,
+                inputTxt: inputTxt
+        };
+})();
 
-	buf += data;
 
-	if (!logging) {
-		if (buf.match(/Breakpoint 1, main \(\)(.|\n)*\(gdb\) $/g) > -1) {
-			// TODO: print initialized data
-			logging = true;
-			buf = "";
-			gdb.stdin.write('step\n');
-		}
-		return;
-	}
 
-	if (buf.match(/\n\(gdb\) $/g)) {
-		buf = buf.replace(/\n\(gdb\) $/g, '');
-		
-		if (mode == 0) {
-			buf = "";
-			mode = 1;
-			gdb.stdin.write('info line\n');
-			return;
-		}
+const gdb = spawn('gdb', [argv.binaryfile]);
 
-		if (mode == 1) {
-			if (buf.indexOf('test.cpp') > -1) {
-				buf = "";
-				mode = 2;
-				gdb.stdin.write('l\n');
-			}
-			else {
-				buf = "";
-				mode = 0;
-				gdb.stdin.write('finish\n');
-			}
-			return;
-		}
 
-		if (mode == 2) {
-			log.write("line:\n" + buf + "\n");
-			buf = "";
-			mode = 3;
-			gdb.stdin.write('display\n');
-			return;
-		}
+(function gdbConfigurationAndRun(argv) {
+        console.log(argv);
+        argv.targets.forEach((target) => {
+                gdb.stdin.write(`display ${target}\n`);
+        });
 
-		if (mode == 3) {
-			log.write("display:\n" + buf + "\n");
-			buf = "";
-			mode = 0;
-			gdb.stdin.write('step\n');
-			return;
-		}
-	}
-});
+        gdb.stdin.write('set listsize 1\n');
+        gdb.stdin.write('break main\n');
 
-gdb.stderr.on('data', (data) => {
-	if (data.toString().indexOf("libc-start.c") > -1) {
-		run = false;
-		gdb.stdin.write('Quit\n');
-	}
-});
+        argv.breaks.forEach((breakpoint) => {
+                console.log(`break ${breakpoint}`);
+        });
 
-gdb.on('close', (code) => {
-	console.log(`child process exited with code ${code}`);
-});
+        gdb.stdin.write(`run < ${argv.inputTxt}\n`);
+})(argv);
+
+
+
+(function gdbProcessing() {
+        const log = fs.createWriteStream('output.log', {flag: 'w'});
+        var logging = false;
+        var run = true;
+        var buf = new String();
+        var mode = 0;
+
+        gdb.stdout.on('data', (data) => {
+                if (!run) return;
+
+                buf += data;
+
+                if (!logging) {
+                        if (buf.match(/Breakpoint 1, main \(\)(.|\n)*\(gdb\) $/g) > -1) {
+                                logging = true;
+                                buf = "";
+                                gdb.stdin.write('step\n');
+                        }
+                        return;
+                }
+
+                if (buf.match(/\n\(gdb\) $/g)) {
+                        buf = buf.replace(/\n\(gdb\) $/g, '');
+
+                        if (mode == 0) {
+                                buf = "";
+                                mode = 1;
+                                gdb.stdin.write('info line\n');
+                                return;
+                        }
+
+                        if (mode == 1) {
+                                if (buf.indexOf('test.cpp') > -1) {
+                                        buf = "";
+                                        mode = 2;
+                                        gdb.stdin.write('l\n');
+                                }
+                                else {
+                                        buf = "";
+                                        mode = 0;
+                                        gdb.stdin.write('finish\n');
+                                }
+                                return;
+                        }
+
+                        if (mode == 2) {
+                                log.write("line:\n" + buf + "\n");
+                                buf = "";
+                                mode = 3;
+                                gdb.stdin.write('display\n');
+                                return;
+                        }
+
+                        if (mode == 3) {
+                                log.write("display:\n" + buf + "\n");
+                                buf = "";
+                                mode = 0;
+                                gdb.stdin.write('step\n');
+                                return;
+                        }
+                }
+        });
+
+        gdb.stderr.on('data', (data) => {
+                if (data.toString().indexOf("libc-start.c") > -1) {
+                        run = false;
+                        gdb.stdin.write('Quit\n');
+                }
+        });
+
+        gdb.on('close', (code) => {
+                console.log(`child process exited with code ${code}`);
+        });
+})();
