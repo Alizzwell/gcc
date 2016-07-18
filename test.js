4c99cae5979880e8b2e987383e2a885e5bc02778
hehe717 <nawonjae@naver.com>
2016-07-18 14:57:58 +0900

    #4 break point 사용자 지정 기능 완료.

--- test.js
+++ test.js
@@ -143,7 +143,7 @@ function gdbConfigurationAndRun(next) {
 
 		// Configuration
 		argv.breaks.forEach((breakpoint) => {
-			// TODO: use break point mode..
+			gdb.stdin.write(`break ${breakpoint}\n`);
 		});
 
 		gdb.stdin.write(`run < ${argv.inputTxt}\n`);
@@ -164,9 +164,15 @@ function gdbProcessing(next) {
 
 		var dataListener = (data) => {
 			buf += data;
-			if (output = getCompleteStreamData(buf)) {
+
+			if (output = getCompleteStreamData(buf)) {			
 				buf = "";
 
+				if (output.indexOf("exited normally") > -1){
+					gdb.stdout.removeListener('data', dataListener);
+					gdb.stdin.write('Quit\n');
+				}
+				
 				if (mode == 0) {
 					mode = 1;
 					gdb.stdin.write('info line\n');
@@ -196,7 +202,13 @@ function gdbProcessing(next) {
 					status = parseStatusEachValues(output, result.targets);
 					steps.push({ "line": line, "status": status });
 					mode = 0;
-					gdb.stdin.write('step\n');
+					if(argv.breaks.length==0){
+						 gdb.stdin.write('step\n');
+					}
+					else{
+						gdb.stdin.write('continue\n');
+						//console.log(data.toString());
+					}
 					return;
 				}
 			}
